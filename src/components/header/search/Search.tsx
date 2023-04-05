2f3920a833962d7350a9ef426c181add610ae97a
Daria Romanova <d.e.romanova@mail.ru>
2023-04-05 17:32:26 +0300

    в компонент поиска добавлена логика состояния

--- src/components/header/search/Search.tsx
+++ src/components/header/search/Search.tsx
@@ -1,16 +1,31 @@
+import { ChangeEvent, useState } from 'react';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import styles from '@/styles/header/Search.module.scss';
 
 function Search() {
+    const [value, setValue] = useState('');
+    
+    function handleChange (event: ChangeEvent<HTMLInputElement>) {
+        setValue(event.target.value);
+    }
+
+    console.log(value);
+
     return (
         <>
             {/* Search visible on desktop */}
             <InputGroup size='sm' className='d-none d-lg-flex ps-2 ps-xxl-5'>
                 <div className={styles.input__frame}>
                     <i className='fi-search position-absolute text-muted top-50 translate-middle-y ms-2'></i>
-                    <input type='search' className={"border-top-0 border-start-0 border-end-0 border-bottom-0 rounded-0 form-control " + styles.input__field} placeholder='Поиск' />
+                    <input
+                        type='search'
+                        className={"border-top-0 border-start-0 border-end-0 border-bottom-0 rounded-0 form-control " + styles.input__field}
+                        placeholder='Поиск'
+                        value = {value}
+                        onChange={handleChange}
+                    />
                     <Button className={styles.input__btn}>Искать</Button>
                 </div>
             </InputGroup>
@@ -30,4 +45,3 @@ function Search() {
 }
 
 export default Search;
-
