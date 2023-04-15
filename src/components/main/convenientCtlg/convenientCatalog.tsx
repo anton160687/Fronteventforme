a1906912da7892da58155bc35d2430332ffb4345
Vin.S <gift121279@gmail.com>
2023-04-15 12:52:06 +0300

    4

--- src/components/main/convenientCtlg/convenientCatalog.tsx
+++ src/components/main/convenientCtlg/convenientCatalog.tsx
@@ -1,12 +1,13 @@
 import { cards } from "@/mocks/cards";
 import { FC } from "react";
-import {Container, Col, Row} from "react-bootstrap";
+import {Container, Row} from "react-bootstrap";
 import ImageLoader from '../../_finder/ImageLoader';
 import { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import Link from "next/link";
 
 export const ConvenientCatalog:FC = () => {
 const {cardsLinkArrey} = cards || {};
@@ -18,11 +19,16 @@ if (!cardsLinkArrey) {
   return(
   <section>
     <Container style={{marginTop: "124px"}}>
-      <Row className="col-xl-6"> 
-        
-      </Row>
-      <Row className="col-xl-6">
-        <figure className="card border-0 align-items-center ">
+      <Row> 
+        <Link href="#" className="icon-box card card-body border-0 card-hover col-xl-6">
+          <div className="icon-box-media text-primary mb-3 me-auto">
+            <i className="fi-makeup"></i>
+          </div>
+          <h3 className="icon-box-title fs-base mb-0">Свадебные платья</h3>
+          <p>Платья от А до Я для каждой невесты</p>
+        </Link>
+
+        <figure className="card border-0 align-items-center col-xl-6">
           <figcaption className="card-body p-0" >
             <h2 className="card-title mb-3">Удобный каталог</h2>
             <p className="card-text mb-4">Более подробное описание категории: важные фильтры, советы и др. We have the most comprehensive directory of estate agents to help you with all your property needs. Whether buying, selling or renting start your search.</p>
@@ -39,14 +45,8 @@ if (!cardsLinkArrey) {
             <SwiperSlide className='d-flex'>
               <ImageLoader src='/img/card/convenient-ctlg/shoreSea.png' width={636} height={462} alt='Image' />
             </SwiperSlide>
-            <SwiperSlide className='d-flex'>
-              <ImageLoader src='/img/card/convenient-ctlg/marriageProposal.png' width={636} height={462} alt='Image' />
-            </SwiperSlide>
-            <SwiperSlide className='d-flex'>
-              <ImageLoader src='/img/card/convenient-ctlg/city.png' width={636} height={462} alt='Image' />
-            </SwiperSlide>
           </Swiper>
-      </figure>
+        </figure>
       </Row>
     </Container>
   </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Hero from '@/components/main/hero/Hero';
 import SupplierCard from '@/components/main/supplierCard/supplierCard';
 import PlanWeddingCard from '@/components/main/planWeddingCard/planWeddingCard';
-import { PersonServices } from '@/components/main/cardIndividualApproach';
 import SupplierSlider from "@/components/main/supplierSlider/supplierSlider";
 import MoreServices from "@/components/main/moreServices/moreServices";
-import { CardsLink } from '@/components/main/cardsLink/cardsLink';
 import {Locations} from "@/components/main/locations/locations";
 import {properties} from "@/mocks/locations";
+import { PersonServices, CardsLink, ConvenientCatalog } from '@/components/main';
 
 
 export default function Home() {  
@@ -15,6 +14,7 @@ export default function Home() {
       <>
           <Hero/>
           <CardsLink/>
+          <ConvenientCatalog/>
           <Locations array={properties} title={"ТОП-5 площадок разных категорий г. Москва"}/>
           <MoreServices/>
           <SupplierSlider/>
