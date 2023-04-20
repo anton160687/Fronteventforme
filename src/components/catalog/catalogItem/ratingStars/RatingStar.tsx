c2638e2654daf46755b281c423846619a31fa1e6
Daria Romanova <d.e.romanova@mail.ru>
2023-04-20 13:05:14 +0300

    добавлен элемент рейтинг, с отображением звезд

--- src/components/catalog/catalogItem/ratingStars/RatingStar.tsx
+++ src/components/catalog/catalogItem/ratingStars/RatingStar.tsx
@@ -1,55 +1,48 @@
 import styles from "@/styles/catalog/places/Places.module.scss";
+import { useEffect, useRef } from "react";
 
+type RatingStarsProps = {
+  rating: number,
+  voices: number,
+}
 
-function RatingStars() {
-    function renderStars () {
+function RatingStars({ rating, voices }: RatingStarsProps) {
+  
+  function renderStars(rating: number): JSX.Element | undefined {
+      while (rating >= 1) {
+        rating--;
+        return (<><svg width="32" height="32" fill="#fed94b"> <use xlinkHref="#star"></use> </svg> {renderStars(rating)}</>)
+      }
 
-        return <>
-
-
-        <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg">
-          <defs>
-            
-          <defs>
-          <linearGradient id="half" x1="0" x2="100%" y1="0" y2="0">
-            <stop offset="50%" stop-color="#fed94b"></stop>
-            <stop offset="50%" stop-color="#f7efc5"></stop>
-          </linearGradient>
-       </defs>
-            
-            <symbol id="star" viewBox="0 0 32 32">
-              <path d="M31.547 12a.848.848 0 00-.677-.577l-9.427-1.376-4.224-8.532a.847.847 0 00-1.516 0l-4.218 8.534-9.427 1.355a.847.847 0 00-.467 1.467l6.823 6.664-1.612 9.375a.847.847 0 001.23.893l8.428-4.434 8.432 4.432a.847.847 0 001.229-.894l-1.615-9.373 6.822-6.665a.845.845 0 00.214-.869z" />
-            </symbol>
-          </defs>
-        </svg>
-    
-        <p className={styles.crate}>
-          <svg className={styles.cicon} width="32" height="32">
-            <use xlinkHref="#star"></use>
-          </svg>
-          <svg className={styles.cicon} width="32" height="32">
-            <use xlinkHref="#star"></use>
-          </svg>
-          <svg className={styles.cicon} width="32" height="32">
-            <use xlinkHref="#star"></use>
-          </svg>
-          <svg className={styles.cicon} width="32" height="32">
-            <use xlinkHref="#star" mask='url(#half)'></use>
-          </svg>
-          <svg className={styles.cicon} width="32" height="32">
-            <use xlinkHref="#star" fill="url(#half)"></use>
-          </svg>
-        </p>
-    
-        </>
-
-
-
+      if (rating < 1 && rating > 0.4) {
+        return (
+          <svg width="32" height="32"> <use xlinkHref="#star" fill="url(#half)"></use> </svg>
+        )
+      }
     }
 
-
-    return (
-        <>
-        </>
-    )
+  return (
+    <>
+    <h3 className={styles.rating__number}>{rating}</h3>
+      <p className={styles.rating__stars}>
+        {renderStars(rating)}
+      </p>
+      <p className={styles.rating__text}>{voices} отзывов</p>
+      <svg width="0" height="0" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <defs>
+            <linearGradient id="half" x1="0" x2="100%" y1="0" y2="0">
+              <stop offset="50%" stop-color="#fed94b"></stop>
+              <stop offset="50%" stop-color="#f7efc5"></stop>
+            </linearGradient>
+          </defs>
+          <symbol id="star" viewBox="0 0 32 32" >
+            <path d="M31.547 12a.848.848 0 00-.677-.577l-9.427-1.376-4.224-8.532a.847.847 0 00-1.516 0l-4.218 8.534-9.427 1.355a.847.847 0 00-.467 1.467l6.823 6.664-1.612 9.375a.847.847 0 001.23.893l8.428-4.434 8.432 4.432a.847.847 0 001.229-.894l-1.615-9.373 6.822-6.665a.845.845 0 00.214-.869z" />
+          </symbol>
+        </defs>
+      </svg>
+    </>
+  )
 }
+
+export default RatingStars;
--- src/pages/catalog/places/[id].tsx
+++ src/pages/catalog/places/[id].tsx
@@ -8,6 +8,7 @@ import YaComments from "../../../components/catalog/catalogItem/yaComments/YaCom
 import AnchorBtns from "@/components/catalog/catalogItem/anchorBtns/AnchorBtns";
 import { LocationPhotos } from "@/components/catalog/catalogItem/locationPhotos/locationsPhotos";
 import styles from "@/styles/catalog/places/Places.module.scss";
+import RatingStars from "@/components/catalog/catalogItem/ratingStars/RatingStar";
 
 type CatalogItemProps = {
   item?: Place,
@@ -24,81 +25,89 @@ export default function CatalogItem({ item }: CatalogItemProps) {
         <Breadcrumb.Item active>{item?.title}</Breadcrumb.Item>
       </Breadcrumb>
 
-      {!item ?
-        <p>Loading...</p>
-        :
-        <>
-          {/* тестовые данные для разных ситуаций, потом - удалить */}
-          <LocationPhotos photoUrls={[
-            'https://picsum.photos/369/224',
-            'https://picsum.photos/369/224',
-            'https://picsum.photos/369/224',
-            'https://picsum.photos/369/224',
-            'https://picsum.photos/369/224',
-          ]} />
+      {/* тестовые данные для разных ситуаций, потом - удалить */}
+      <LocationPhotos photoUrls={[
+        'https://picsum.photos/369/224',
+        'https://picsum.photos/369/224',
+        'https://picsum.photos/369/224',
+        'https://picsum.photos/369/224',
+        'https://picsum.photos/369/224',
+      ]} />
 
+      {/* это - общий контейнер страницы на все блоки под верхними фото */}
+      <div  className={styles.location__flex_container}>
+
+        {/* это - боковой контейнер справа на странице */}
+        <div>
+        <RatingStars rating={3.7 } voices={58} />
+        {/* <RatingStars rating={item?.rating?.rating || 0 } voices={item?.rating?.votes || 0} /> */}
+
+        </div>
+        {/* это - основной контейнер слева на странице */}
+        <div className={styles.left_container}>
           <div className={styles.location__flex_container}>
-            <h3>{item.title}</h3>
+            <h3>{item?.title}</h3>
             <p>3 зала 2 веранды 2 шатра</p>
           </div>
 
           <div className={styles.location__flex_container}>
-            <p>{item.address.full}</p>
+            <p>{item?.address.full}</p>
             <Link href='#' className={styles.location__map}>
               <i className="fi-map" /> <p>На карте</p>
             </Link>
           </div>
 
-        </>
-      }
 
-      <AnchorBtns />
-      {/* все отсутстсвующие поля должны приходить с бэка */}
-      <h3>Описание:</h3>
-      <div className={styles.location__description_container}>
-        <ul className={styles.location__description_container_ul}>
-          <li className={styles.location__description_container_li}>Вместимость <span>{item?.capacity || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Расположение <span>{item?.placement || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Кухня <span>{item?.cuisine || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Время работы <span>{item?.hours || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Свой алкоголь <span>{item?.alcohol ? "Разрешено" : "Запрещено"}</span></li>
-        </ul>
-        <ul className={styles.location__description_container_ul}>
-          <li className={styles.location__description_container_li}>Аренда <span>{item?.lease || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Средний чек <span>{item?.avg_price || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Пробковый сбор <span>{item?.fee || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Депозит <span>{item?.deposit || 'нет информации'}</span></li>
-          <li className={styles.location__description_container_li}>Продление <span>{item?.continue || 'нет информации'}</span></li>
-        </ul>
+          <AnchorBtns />
+          {/* все отсутстсвующие поля должны приходить с бэка */}
+          <h3>Описание:</h3>
+          <div className={styles.location__description_container}>
+            <ul className={styles.location__description_container_ul}>
+              <li className={styles.location__description_container_li}>Вместимость <span>{item?.capacity || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Расположение <span>{item?.placement || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Кухня <span>{item?.cuisine || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Время работы <span>{item?.hours || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Свой алкоголь <span>{item?.alcohol ? "Разрешено" : "Запрещено"}</span></li>
+            </ul>
+            <ul className={styles.location__description_container_ul}>
+              <li className={styles.location__description_container_li}>Аренда <span>{item?.lease || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Средний чек <span>{item?.avg_price || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Пробковый сбор <span>{item?.fee || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Депозит <span>{item?.deposit || 'нет информации'}</span></li>
+              <li className={styles.location__description_container_li}>Продление <span>{item?.continue || 'нет информации'}</span></li>
+            </ul>
+          </div>
+
+          <h3>Подходит для:</h3>
+          <ul>
+            <li>Свадьба</li>
+            <li>День рождения</li>
+            <li>Новый год</li>
+          </ul>
+          <ul>
+            <li>Фуршет</li>
+            <li>Мальчишник</li>
+            <li>Девичник</li>
+          </ul>
+          <ul>
+            <li> Выпускной</li>
+            <li> Корпоратив</li>
+            <li> Праздничный банкет</li>
+          </ul>
+          <h3>Детали о кухне площадки:</h3>
+          <p>Европейская, русская, кавказская кухня</p>
+          <p>Есть детское меню</p>
+          <div id="map">
+            Здесь карта Яндекса с объектом
+          </div>
+
+          <div id="comments">
+            <YaComments />
+          </div>
+
+        </div>
+
       </div>
-
-      <h3>Подходит для:</h3>
-      <ul>
-        <li>Свадьба</li>
-        <li>День рождения</li>
-        <li>Новый год</li>
-      </ul>
-      <ul>
-        <li>Фуршет</li>
-        <li>Мальчишник</li>
-        <li>Девичник</li>
-      </ul>
-      <ul>
-        <li> Выпускной</li>
-        <li> Корпоратив</li>
-        <li> Праздничный банкет</li>
-      </ul>
-      <h3>Детали о кухне площадки:</h3>
-      <p>Европейская, русская, кавказская кухня</p>
-      <p>Есть детское меню</p>
-      <div id="map">
-        Здесь карта Яндекса с объектом
-      </div>
-
-      <div id="comments">
-        <YaComments />
-      </div>
-
     </Container>
   )
 }
