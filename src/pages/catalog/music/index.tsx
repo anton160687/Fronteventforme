13794eb43b9f61a035e7e3ae76ac4472f192929d
Daria Romanova <d.e.romanova@mail.ru>
2023-04-26 19:05:38 +0300

    исправлен по новому макету сайдбар каталога

--- src/pages/catalog/music/index.tsx
+++ src/pages/catalog/music/index.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Container from "react-bootstrap/Container";
-
+import { Col } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Sidebar from "@/components/catalog/sidebar/Sidebar";
 
 export default function Catalog() {
   return (
@@ -12,7 +14,12 @@ export default function Catalog() {
         <Breadcrumb.Item linkAs={Link} href='/catalog'>Каталог</Breadcrumb.Item>
         <Breadcrumb.Item active>Музыка</Breadcrumb.Item>
       </Breadcrumb>
-
+      <Row>
+        <Sidebar />
+        <Col>
+          <h2>Музыка</h2>
+        </Col>
+      </Row>
     </Container>
   )
 }
--- src/pages/catalog/places/index.tsx
+++ src/pages/catalog/places/index.tsx
@@ -13,7 +13,7 @@ import { selectPlaces, setPlaces } from "@/store/catalog/catalogSlice";
 import Sorting from "@/components/catalog/sorting/Sorting";
 import PlaceFilters from "@/components/catalog/placeFilters/PlaceFilters";
 import { CatalogPlaceCard, TopSlidersPlaces } from "@/components/catalog/";
-import styles from '@/styles/catalog/places/Places.module.scss';
+
 //для SSR
 import { URL } from "@/constant";
 import { Place } from "@/types/catalog";
@@ -62,40 +62,40 @@ function CatalogPlaces({ places }: CatalogPlacesProps) {
         <Breadcrumb.Item active>Площадки</Breadcrumb.Item>
       </Breadcrumb>
 
-      <Row>
+      <Row className="p-0">
         <Title title={'Площадки'} quantity={places.length} />
         <TopSlidersPlaces />
         <PlaceFilters />
+      </Row>
 
-        <Row className="p-0">
-          <Sidebar />
-          <Col className="ms-4 p-0">
-            <Sorting sortingCB={sortPlacesByParam} />
+      <Row className="p-0">
+        <Sidebar />
 
-            <section>
-              {sortedPlaces ?
-                renderAllPlaces(sortedPlaces)
-                :
-                renderAllPlaces(places)
-              }
-            </section>
+        <Col className="ms-4 p-0">
+          <Sorting sortingCB={sortPlacesByParam} />
+          <section>
+            {sortedPlaces ?
+              renderAllPlaces(sortedPlaces)
+              :
+              renderAllPlaces(places)
+            }
+          </section>
 
-            <Pagination size='lg'>
-              <Pagination.Item>
-                <i className='fi-chevron-left'></i>
-              </Pagination.Item>
-              <Pagination.Item>{1}</Pagination.Item>
-              <Pagination.Item active>{2}</Pagination.Item>
-              <Pagination.Item>{3}</Pagination.Item>
-              <Pagination.Ellipsis />
-              <Pagination.Item>{10}</Pagination.Item>
-              <Pagination.Item>
-                <i className='fi-chevron-right'></i>
-              </Pagination.Item>
-            </Pagination>
-          </Col>
+          <Pagination size='lg'>
+            <Pagination.Item>
+              <i className='fi-chevron-left'></i>
+            </Pagination.Item>
+            <Pagination.Item>{1}</Pagination.Item>
+            <Pagination.Item active>{2}</Pagination.Item>
+            <Pagination.Item>{3}</Pagination.Item>
+            <Pagination.Ellipsis />
+            <Pagination.Item>{10}</Pagination.Item>
+            <Pagination.Item>
+              <i className='fi-chevron-right'></i>
+            </Pagination.Item>
+          </Pagination>
 
-        </Row>
+        </Col>
       </Row>
 
       <BotomFilters />
