ed625c999c03700b5e329eae951ea10cf338a3eb
Daria Romanova <d.e.romanova@mail.ru>
2023-04-27 14:34:32 +0300

    добавлено сохранение токена и отпрака на бэк (тестовый url)

--- src/pages/user_activation/[uid]/[token].tsx
+++ src/pages/user_activation/[uid]/[token].tsx
@@ -3,27 +3,52 @@ import { useEffect } from 'react';
 import { Container, Spinner } from "react-bootstrap";
 import styles from '@/styles/sign/Sign.module.scss';
 
+
 export default function finishRegistrationPage() {
     const router = useRouter();
     //здесь ловим динамические параметры из адресной строки
-    const {uid, token} = router.query;
-    //пока что оставить логирование для проверки правильности данных, потом удалить
-    console.log(router.query);
-    console.log(uid);
-    console.log(token);
+    const uid = router.query.uid as string;
+    const token = router.query.token as string;
 
-    const handleRedirect = () => {
+    async function sendAuthData() {
+        let data = {
+            uid: uid,
+            token: token
+        }
+        console.log(data);
+        //пока что URL в хардкоде, потом надо будет поместить рабочее URL в env
+        //и, поскольку это нерабочее URL, сейчас запрос уходит вникуда и возвращает 404, но это пока нормально
+        let response = await fetch('127.0.0.1:8000/auth/users/activation/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        }
+        )
+        //стоит подумать о том, какие здесь м.б. обработчики ошибок. Пока что просто логирование
+        if (response.ok) {
+            console.log('авторизация прошла успешно');
+        } else {
+            console.log(response);
+        }
+    }
+
+    function handleRedirect() {
         router.push('/')
-      }
+    }
 
-    useEffect(()=> {
+    useEffect(() => {
         if (uid && token) {
+            //сохраняем токен в localStorage
+            localStorage.setItem("token", token);
             // здесь - отправка данных на бэк
+            sendAuthData();
+            //перенаправляем на главную или иную страницу, пока отключено
             // handleRedirect();
         }
-        
     }, [uid, token])
-    
+
     return (
         <Container className={styles.auth__container} >
             <Spinner className={styles.auth__spinner} />
