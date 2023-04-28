173a6e6ee4ce5d7dc09027b9aabedf8782f524eb
Ksenia <naomi355@yandex.ru>
2023-04-29 00:06:53 +0400

    добавила форму для рассчета стоимости

--- src/components/catalog/catalogItem/catalogItemSlider/CatalogItemSlider.tsx
+++ src/components/catalog/catalogItem/catalogItemSlider/CatalogItemSlider.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, ButtonGroup, ToggleButton } from 'react-bootstrap';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -16,11 +16,14 @@ import 'lightgallery/css/lg-fullscreen.css';
 import 'lightgallery/css/lg-video.css';
 import { useState } from 'react';
 import Badge from 'react-bootstrap/Badge';
-
 import DatePicker from 'react-datepicker';
+import { addDays } from 'date-fns';
 import 'react-datepicker/dist/react-datepicker.css';
 import Form from 'react-bootstrap/Form';
 import { Hall } from '@/types/catalog';
+
+import Modal from 'react-bootstrap/Modal';
+import CloseButton from 'react-bootstrap/CloseButton';
 
 const places: Hall[] = [
   {
@@ -50,6 +53,7 @@ const places: Hall[] = [
 ];
 
 export default function CatalogItemSlider(): JSX.Element {
+  //отображение "Показать еще"
   const [isDetailsOpen, setIsDetailsOpen] = useState(false);
 
   function detailsRender(description: string): JSX.Element {
@@ -85,7 +89,36 @@ export default function CatalogItemSlider(): JSX.Element {
       </div>
     </div>
   );
-  const [startDate, setStartDate] = useState(null);
+
+  //даты первого и последнего дня бронирования
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+    console.log('dates', dates);
+  };
+
+  const [isCalendarShown, setIsCalendarShown] = useState(false);
+  //для отображения формы "рассчитать стоимость"
+  const [isCalcShown, setIsCalcShown] = useState(false);
+  // Property cost calculator modal
+  const [modalShow, setModalShow] = useState(false);
+  const handleModalClose = () => setModalShow(false);
+  const handleModalShow = () => setModalShow(true);
+
+  // Form validation
+  const [validated, setValidated] = useState(false);
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
 
   return (
     <>
@@ -98,29 +131,118 @@ export default function CatalogItemSlider(): JSX.Element {
           key={index}
         >
           <h4 className="h4 text-weight-bold">{place.title}</h4>
-
-          <div className="d-flex" style={{ fontWeight: '500' }}>
-            <button>
+          <div className="d-flex mb-3" style={{ fontWeight: '500' }}>
+            <button className={styles.calc_btn} onClick={handleModalShow}>
               <i className="fi-calculator fs-lg me-1"></i>Рассчитать стоимость
             </button>
-            <Form.Group controlId="date-min-max-input">
-              <Form.Label>
+            {/* Property cost calculator modal */}
+            <Modal centered show={modalShow} onHide={handleModalClose}>
+              <Modal.Header className="d-block position-relative border-0 pb-0 px-sm-5 px-4">
+                <Modal.Title as="h4" className="mt-4 text-center">
+                  Рассчитать стоимость
+                </Modal.Title>
+                <CloseButton
+                  onClick={handleModalClose}
+                  aria-label="Закрыть окно"
+                  className="position-absolute top-0 end-0 mt-3 me-3"
+                />
+              </Modal.Header>
+              <Modal.Body className="px-sm-5 px-4">
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                  <Form.Group controlId="property-city" className="mb-3">
+                    <Form.Label className="fw-bold mb-2">
+                      Property location
+                    </Form.Label>
+                    <Form.Select required>
+                      <option value="">Choose city</option>
+                      <option value="Chicago">Chicago</option>
+                      <option value="Dallas">Dallas</option>
+                      <option value="Los Angeles">Los Angeles</option>
+                      <option value="New York">New York</option>
+                      <option value="San Diego">San Diego</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                      Please choose the city.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Select required>
+                      <option value="">Choose district</option>
+                      <option value="Brooklyn">Brooklyn</option>
+                      <option value="Manhattan">Manhattan</option>
+                      <option value="Staten Island">Staten Island</option>
+                      <option value="The Bronx">The Bronx</option>
+                      <option value="Queens">Queens</option>
+                    </Form.Select>
+                    <Form.Control.Feedback type="invalid">
+                      Please choose the district.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group
+                    controlId="property-address"
+                    className="pt-2 mb-3"
+                  >
+                    <Form.Label className="fw-bold mb-2">Address</Form.Label>
+                    <Form.Control placeholder="Enter your address" required />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide your property&apos;s address.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+
+                  <Form.Group controlId="property-area" className="pt-2 mb-4">
+                    <Form.Label className="fw-bold mb-2">
+                      Total area, sq.m.
+                    </Form.Label>
+                    <Form.Control
+                      placeholder="Enter your property area"
+                      required
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please enter your property&apos;s area.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Button type="submit" variant="primary d-block w-100 mb-4">
+                    <i className="fi-calculator me-2"></i>
+                    Рассчитать
+                  </Button>
+                </Form>
+              </Modal.Body>
+            </Modal>
+
+            <div className={styles.calendar_wrapper}>
+              <button
+                onClick={() => setIsCalendarShown((prev) => !prev)}
+                className={styles.busy_btn}
+              >
                 <i className="fi-calendar fs-lg me-1"></i>Проверить занятость
-              </Form.Label>
-              <Form.Control
-                style={{ display: 'none' }}
-                as={DatePicker}
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
-                minDate={new Date('02/01/2022')}
-                maxDate={new Date('02/29/2022')}
-                placeholderText="Select a date in Feb 2022"
-                className="rounded pe-5"
-              />
-            </Form.Group>
-            {/* <button>
-              <i className="fi-calendar fs-lg me-1"></i>Проверить занятость
-            </button> */}
+              </button>
+              <div
+                className={
+                  isCalendarShown ? styles.calendar : styles.calendar_none
+                }
+              >
+                <DatePicker
+                  selected={startDate}
+                  onChange={onChange}
+                  minDate={new Date(Date.now())}
+                  maxDate={new Date('02/29/2024')}
+                  placeholderText="Select a date in Feb 2022"
+                  className="rounded-1 pe-5"
+                  endDate={endDate}
+                  startDate={startDate}
+                  // массив занятых дат, пока в качестве примера, будет приходить из бд
+                  excludeDates={[
+                    addDays(new Date(), 1),
+                    addDays(new Date(), 5),
+                    addDays(new Date(), 10),
+                    addDays(new Date(), 11),
+                    addDays(new Date(), 12),
+                  ]}
+                  selectsRange
+                  inline
+                />
+              </div>
+            </div>
           </div>
           <LightGallery
             selector=".gallery-item"
@@ -144,10 +266,10 @@ export default function CatalogItemSlider(): JSX.Element {
                 }}
                 data-carousel-options='{"loop": true}'
                 onSlideChange={(swiper) => {
-                  setCurrentSlide(swiper.realIndex + 1);
+                  setCurrentSlide(swiper.realIndex + 1 + 2);
                 }}
                 onInit={(swiper) => {
-                  setCurrentSlide(swiper.realIndex + 1);
+                  setCurrentSlide(swiper.realIndex + 1 + 2);
                   setTotalSlides(swiper.slides.length);
                 }}
               >
@@ -235,6 +357,8 @@ export default function CatalogItemSlider(): JSX.Element {
               </div>
               <div className={styles.sale}>
                 <p style={{ fontWeight: '500' }}>{place.sale.condition}</p>
+
+                {/* //! тут по ТЗ: при нажатии открывается карусель фото комнаты, но я пока сделала бэйдж до прояснения вопроса (дизайнер тоже бэйдж сделала) */}
                 <Badge className={styles.badge}>
                   <i className="fi-gift fs-lg me-1"></i>
                   {place.sale.btn}
