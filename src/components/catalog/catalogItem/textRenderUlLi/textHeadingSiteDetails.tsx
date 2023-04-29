d5beb7212e42f75a47e730dce621f4c6976cf271
Vin.S <gift121279@gmail.com>
2023-04-29 13:33:22 +0300

    изменил - Детали площадки -

--- src/components/catalog/catalogItem/textRenderUlLi/textHeadingSiteDetails.tsx
+++ src/components/catalog/catalogItem/textRenderUlLi/textHeadingSiteDetails.tsx
@@ -1,17 +1,25 @@
-import { FC } from "react"
-import { Button, Card } from "react-bootstrap"
-import styles from "@/styles/catalog/places/Places.module.scss";
-import Link from "next/link";
+import { FC, useState } from "react"
+import { Card } from "react-bootstrap"
 
-export const TextHeadingSiteDetails:FC = () => (
+const description = 'Тип площадки: банкетный зал, веранда, гостиница/отель, летняя площадка, банкетный комплекс, терраса, загородный комплекс, шатер Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt temporibus non, magnam praesentium eum assumenda ad ex doloremque, harum, vero eveniet numquam voluptatibus. Ipsa, enim ipsum provident iure veritatis laudantium totam eum eligendi laboriosam! Dolorem quam necessitatibus, unde laudantium ut, est, assumenda quasi expedita iure maiores deleniti repellendus? Earum sequi non rerum incidunt nostrum, architecto, autem doloribus assumenda quibusdam debitis ea corporis sint dolores nihil tenetur vitae odio necessitatibus reiciendis corrupti exercitationem. Tempora, ullam eius odio dolore, neque nesciunt nihil corrupti, excepturi ex sequi cumque culpa nisi in laborum sapiente minus distinctio ducimus necessitatibus quas quis ipsum. Hic culpa iusto ducimus, ipsum labore libero. Veniam eos quasi adipisci officia assumenda, vel qui voluptatibus atque odit deleniti iusto! Quasi aliquid animi laborum repellat illo quod temporibus.'
 
-<Card className='border-0 mb-xl-5 mb-md-4 mb-sm-3'>
+export const TextHeadingSiteDetails:FC = () => {
+  const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+  const new_description = description.slice(0, 128) + '...';
+
+return(<Card className='border-0 mb-xl-5 mb-md-4 mb-sm-3'>
   <Card.Body className='p-0'>
     <Card.Title as='h4' className='mb-3'>Детали площадки</Card.Title>
-    <Card.Text className='mb-2'>
-      Тип площадки: банкетный зал, веранда, гостиница/отель, летняя площадка, банкетный комплекс, терраса, загородный комплекс, шатер
+    <Card.Text className='mb-2' style={isDetailsOpen ? { display: 'none' } : {}}>
+      {new_description}
     </Card.Text>
-    <Link href='#' className='col-sm-2 border-0 text-decoration-none'>Показать еще</Link>
+    <Card.Text className='mb-2' style={!isDetailsOpen ? { display: 'none' } : {}}>
+      {description}
+    </Card.Text>
+    <Card.Text onClick={() => setIsDetailsOpen((prev) => !prev)}
+        className='mb-0 text-primary cursor-pointer'>
+      {isDetailsOpen ? 'Свернуть' : 'Показать еще'}
+    </Card.Text>
   </Card.Body>
 </Card>
-)
+)}
