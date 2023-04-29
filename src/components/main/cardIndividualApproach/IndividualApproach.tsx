b2384fecf842f266ed88935028a3219ca4b0cdf2
Ksenia <naomi355@yandex.ru>
2023-04-29 21:17:07 +0400

    поменяла местами компоненты как в макете

--- src/components/main/cardIndividualApproach/IndividualApproach.tsx
+++ src/components/main/cardIndividualApproach/IndividualApproach.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
-import { FC } from "react";
-import {Container, Col, Row} from "react-bootstrap";
-import { CardName, CardRepetition } from "./cards";
+import Image from 'next/image';
+import { FC } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import { CardName, CardRepetition } from './cards';
+import { MainTitle } from '../title/title';
+import styles from '@/styles/main/Main.module.scss';
 
-export const PersonServices:FC = () => {
-  
+export const PersonServices: FC = () => {
   return (
-  <section>
-    <Container style={{marginTop: "124px"}}>
-      <Row> 
-        <Col sm={12} className='text-center'>
-          <p className='mb-3'>Индивидуальный подход</p>
-          <h1 className='mb-5'>Персонализированные услуги и <br />статьи под любой запрос </h1>
-        </Col>
-      </Row>
-      <Row className='mb-3'>
-        <h2 className="col-lg-12 col-xl-9 text-center">Статьи от свадебных экспертов и реальные свадьбы</h2>
-        <button type="button" className="btn btn-outline-secondary rounded-pill border-0 p-0 col-lg-12 col-xl-3">
+    <Container
+      as="section"
+      className={'mx-auto w-75 pt-2 pt-sm-0 pb-md-2 ' + styles.my124}
+    >
+      <MainTitle
+        suptitle={'Индивидуальный подход'}
+        title={'Персонализированные услуги и статьи под любой запрос'}
+      />
+      <Row className="mb-3">
+        <h2 className="col-lg-12 col-xl-9 text-center">
+          Статьи от свадебных экспертов и реальные свадьбы
+        </h2>
+        <button
+          type="button"
+          className="btn btn-outline-secondary rounded-pill border-0 p-0 col-lg-12 col-xl-3"
+        >
           Перейти в блог<i className="fi-arrow-long-right m-2"></i>
         </button>
-      </Row>      
-      <Row className='mb-4'>
-
-        <CardRepetition src="/img/card/cardsPersonServices/hands.png"/>
+      </Row>
+      <Row className="mb-4">
+        <CardRepetition src="/img/card/cardsPersonServices/hands.png" />
 
         <figure className="card card-hover border-0 align-items-center col-lg-6 col-md-12 col-sm-12">
-          <Image  
-            src="/img/card/cardsPersonServices/sky.png" 
-            className="rounded-2 mb-3" 
-            alt="card image" 
+          <Image
+            src="/img/card/cardsPersonServices/sky.png"
+            className="rounded-2 mb-3"
+            alt="card image"
             width={663}
-            height={324}/>
-          <figcaption className="card-body p-0 col-lg-12 col-md-8 col-sm-11" >
+            height={324}
+          />
+          <figcaption className="card-body p-0 col-lg-12 col-md-8 col-sm-11">
             <p className="card-text text-primary">подпись</p>
-            <h6 className="card-title">Длинное название статьи из шести слов</h6>
-          </figcaption> 
-          <CardName/>     
+            <h6 className="card-title">
+              Длинное название статьи из шести слов
+            </h6>
+          </figcaption>
+          <CardName />
         </figure>
 
-        <CardRepetition src="/img/card/cardsPersonServices/flowers.png"/>
-
+        <CardRepetition src="/img/card/cardsPersonServices/flowers.png" />
       </Row>
-      <Row className='mb-4'>
+      <Row className="mb-4">
         <figure className="card card-hover border-0 align-items-center col-lg-6 col-md-12 col-sm-12">
-        <Image  
-          src="/img/card/cardsPersonServices/sky.png" 
-          className="rounded-2 mb-3" 
-          alt="card image" 
-          width={663}
-          height={324}/>
-        <figcaption className="card-body p-0 col-lg-12 col-md-8 col-sm-11" >
-          <p className="card-text text-primary">подпись</p>
-          <h6 className="card-title">Длинное название статьи из шести слов</h6>
-        </figcaption>
-        <CardName/>
-      </figure>
-    
-        <CardRepetition src="/img/card/cardsPersonServices/hands.png"/>
-        <CardRepetition src="/img/card/cardsPersonServices/flowers.png"/>
+          <Image
+            src="/img/card/cardsPersonServices/sky.png"
+            className="rounded-2 mb-3"
+            alt="card image"
+            width={663}
+            height={324}
+          />
+          <figcaption className="card-body p-0 col-lg-12 col-md-8 col-sm-11">
+            <p className="card-text text-primary">подпись</p>
+            <h6 className="card-title">
+              Длинное название статьи из шести слов
+            </h6>
+          </figcaption>
+          <CardName />
+        </figure>
+
+        <CardRepetition src="/img/card/cardsPersonServices/hands.png" />
+        <CardRepetition src="/img/card/cardsPersonServices/flowers.png" />
       </Row>
     </Container>
-  </section>
-)}
-
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,50 @@
 import Hero from '@/components/main/hero/Hero';
 import SupplierCard from '@/components/main/supplierCard/supplierCard';
 import PlanWeddingCard from '@/components/main/planWeddingCard/planWeddingCard';
-import SupplierSlider from "@/components/main/supplierSlider/supplierSlider";
-import MoreServices from "@/components/main/moreServices/moreServices";
-import { Locations } from "@/components/main/locations/locations";
+import SupplierSlider from '@/components/main/supplierSlider/supplierSlider';
+import MoreServices from '@/components/main/moreServices/moreServices';
+import { Locations } from '@/components/main/locations/locations';
 import { Place } from '@/types/catalog';
-import { GetStaticProps } from "next";
+import { GetStaticProps } from 'next';
 import { URL } from '@/constant';
-import { PersonServices, CardsLink, ConvenientCatalog } from '@/components/main';
+import {
+  PersonServices,
+  CardsLink,
+  ConvenientCatalog,
+} from '@/components/main';
 
 type HomeProps = {
-  topLocations: Place[],
-}
+  topLocations: Place[];
+};
 
-export default function Home({topLocations}: HomeProps) {
+export default function Home({ topLocations }: HomeProps) {
   return (
-      <>
-          <Hero/>
-          <CardsLink/>
-          <ConvenientCatalog/>
-          <Locations locations={topLocations} title={"ТОП-5 площадок разных категорий г. Москва"} isShowAll={false}/>
-          <MoreServices/>
-          <SupplierSlider/>
-           <Locations locations={topLocations} title={"Лучшие локации"} isShowAll={true} showAllTxt='Показать все'/>
-          <PersonServices/>
-          <SupplierCard/>
-          <PlanWeddingCard/>
-      </>
-  )
+    <>
+      <Hero />
+      <CardsLink />
+      <ConvenientCatalog />
+      <Locations
+        locations={topLocations}
+        title={'ТОП-5 площадок разных категорий г. Москва'}
+        isShowAll={false}
+      />
+      <PersonServices />
+      <MoreServices />
+      <SupplierSlider />
+      <Locations
+        locations={topLocations}
+        title={'Лучшие локации'}
+        isShowAll={true}
+        showAllTxt="Показать все"
+      />
+
+      <SupplierCard />
+      <PlanWeddingCard />
+    </>
+  );
 }
 
-export const getStaticProps:GetStaticProps = async() => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch(`${URL}/places/`);
   const result: Place[] = await response.json();
   const locations: Place[] = result?.slice(0, 5);
@@ -38,10 +52,10 @@ export const getStaticProps:GetStaticProps = async() => {
   if (!locations) {
     return {
       notFound: true,
-    }
+    };
   }
-  
+
   return {
-    props: {topLocations: locations},
-  }
-}
+    props: { topLocations: locations },
+  };
+};
