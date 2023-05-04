408659aa49ee91d0c5fcc6f72dcb0094ab1ca65e
Daria Romanova <d.e.romanova@mail.ru>
2023-05-04 16:02:17 +0300

    в типы и константы занесены данные, согласованные с бэком для использования в формах

--- src/components/addProperty/areaForm/AreaForm.tsx
+++ src/components/addProperty/areaForm/AreaForm.tsx
@@ -1,18 +1,23 @@
-import { ChangeEvent, useState } from 'react';
-import { ButtonGroup, ToggleButton } from 'react-bootstrap';
+import { ChangeEvent, SetStateAction, useState } from 'react';
+import { ButtonGroup, Dropdown, Row, ToggleButton } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
 import BasicForm from '../basicForm/BasicForm';
+import FileUploader from '../fileUploader/FileUploader';
+import { COLOR_HALL, TYPE_AREA } from '@/constant';
+import Image from 'next/image';
 
 type AreaFormProps = {
     index: number
 }
 
-function AreaForm({ index }: AreaFormProps ) {
+function AreaForm({ index }: AreaFormProps) {
     //название помещения
     const [title, setTitle] = useState<string>('');
     function handleChange(e: ChangeEvent<HTMLInputElement>) {
         setTitle(e.target.value);
     }
+    // Загрузка картинок
+    const [gallery, setGallery] = useState<string[]>([]);
 
     return (
         <section id={`area${index}`} className='card card-body border-0 shadow-sm p-4 mb-4'>
@@ -20,7 +25,55 @@ function AreaForm({ index }: AreaFormProps ) {
                 <i className='fi-party-popper text-primary fs-5 mt-n1 me-2'></i>
                 Помещения
             </h2>
-            <BasicForm title={title} location={false} handleChange={handleChange}/>
+            <BasicForm title={title} location={false} handleChange={handleChange} />
+
+            <FileUploader gallery={gallery} setGallery={setGallery} />
+
+            <Form.Group className='mb-4'>
+                <Form.Label>Тип <span className='text-danger'>*</span></Form.Label>
+                <Dropdown>
+                    <Dropdown.Toggle variant='outline-secondary'>{TYPE_AREA[0][1]}</Dropdown.Toggle>
+                    <Dropdown.Menu className='my-1'>
+                        {TYPE_AREA.map((option, indx) =>
+                            <Dropdown.Item key={indx}>
+                                {option[1]}
+                            </Dropdown.Item>)}
+                    </Dropdown.Menu>
+                </Dropdown>
+
+                <Form.Label>Вместимость <span className='text-danger'>*</span></Form.Label>
+                <Form.Control
+                    placeholder='от 30'
+                    type='number'
+                    min="30"
+                    max="60"
+                    value={0}
+                    onChange={() => { }}
+                    required
+                />
+                <Image src={'/Line.png'} alt={'inbetween'} width={29} height={1}/>
+                <Form.Control
+                    placeholder='от 30'
+                    type='number'
+                    min="30"
+                    max="60"
+                    value={0}
+                    onChange={() => { }}
+                    required
+                />
+
+                <Form.Label>Цвет зала <span className='text-danger'>*</span></Form.Label>
+                <Dropdown>
+                    <Dropdown.Toggle variant='outline-secondary'>{COLOR_HALL[0]}</Dropdown.Toggle>
+                    <Dropdown.Menu className='my-1'>
+                        {COLOR_HALL.map((option, indx) =>
+                            <Dropdown.Item key={indx}>
+                                {option}
+                            </Dropdown.Item>)}
+                    </Dropdown.Menu>
+                </Dropdown>
+
+            </Form.Group>
 
             {/* <Form.Group className='mb-4'>
                 <Form.Label>Расположение <span className='text-danger'>*</span></Form.Label>
---
+++ src/types/placeType.ts
@@ -0,0 +1,43 @@
+export type Place = {
+    title: string,
+    city: string,
+    metro: string,
+    address: string,
+    start_time: Date,
+    finish_time: Date,
+    location: string[],
+    fireworks: boolean,
+    kitchen: string[],
+    children_kitchen: boolean,
+    alco: boolean,
+    scheme_of_payment: string,
+    corkage_fee: boolean,
+    payment_of_alco: number,
+    lease_extension: boolean,
+    lease_extension_price: number,
+    average_check: number,
+    event: string[],
+    description: string
+}
+
+/* с бэка
+title - строка, максимальная длина 50
+city - строка, максимальная длина 20
+metro - строка, 20 символов
+address - строка, 300 символов
+start_time - время (TimeField)
+finish_time - время (TimeField)
+location - m2m с таблицей Location
+fireworks - Логический тип
+kitchen - m2m с таблицей Kitchen
+children_kitchen - логический тип
+alco(можно со своим алко или нет) - логический тип
+scheme_of_payment(схема оплаты) - строка, выпадающий список. 4 символа
+corkage_fee(пробковый сбор) - логический тип
+payment_of_alco(цена за свой алкоголь) - положительное целое число
+lease_extension(продление аренды) - логический тип
+lease_extension_price(стоимость продления) - положительное целое число
+average_check(средний чек) - положительное целое число
+event - m2m с таблицей Event
+description - текст
+*/
