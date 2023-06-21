b96963a75e0293e357f00e751f674a06b8366268
Ksenia <naomi355@yandex.ru>
2023-06-21 21:44:18 +0400

    сделала таймер

--- src/components/sign/signInText/signInText.tsx
+++ src/components/sign/signInText/signInText.tsx
@@ -1,5 +1,7 @@
 import { resetPassword } from '@/store/user/userAPI';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import CountDown from '../countDown/CountDown';
 
 type SignInTextProps = {
   email: string;
@@ -7,42 +9,41 @@ type SignInTextProps = {
 
 export default function SignInText({ email }: SignInTextProps) {
   const [isDisabled, setIsDisabled] = useState(true);
+  const minutes: number = 2;
+
+  const countDownRender = () => {
+    return (
+      <CountDown
+        minutes={minutes}
+        setTimeIsUp={() => {
+          setIsDisabled(false);
+        }}
+      />
+    );
+  };
 
   const handleClick = () => {
     resetPassword(email);
     setIsDisabled(true);
-    setTimeout(() => {
-      //для избежания многократного нажатия на повторное отправление ссылки
-      setIsDisabled(false);
-      //одна минута
-    }, 60000);
   };
-
-  useEffect(() => {
-    setTimeout(() => {
-      //для избежания многократного нажатия на повторное отправление ссылки
-      setIsDisabled(false);
-      //одна минута
-    }, 60000);
-  }, []);
 
   return (
     <div className=" text-center text-lg-start">
       <h4 className="h4 mb-4 text-center text-lg-start">
-        На вашу почту была выслана ссылка для сброса пароля.
+        На вашу почту была выслана ссылка для сброса пароля
       </h4>
-      <p>Если вы не получили письмо, проверьте папку &quot;Спам&quot;</p>
-      <button
-        className={`text-primary border-0 bg-white text-lg-start p-0 ${
-          isDisabled ? 'text-muted' : ''
-        }`}
+      <p>
+        Если вы не получили письмо, советуем проверить папку &quot;Спам&quot;
+      </p>
+      <Button
+        variant="primary"
+        className="w-100"
         onClick={handleClick}
         disabled={isDisabled}
       >
-        {!isDisabled
-          ? 'Выслать ссылку повторно'
-          : 'Повторить действие можно будет через 1 минуту'}
-      </button>
+        {'Выслать ссылку повторно\u00A0'}
+        {isDisabled && countDownRender()}
+      </Button>
     </div>
   );
 }
