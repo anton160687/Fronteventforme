156b1f651edb18c95c5ea8b3bc9b0173d6d769f7
Daria Romanova <d.e.romanova@mail.ru>
2023-06-27 18:38:56 +0300

    начало правок футера

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,153 +1,130 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Image from 'next/image';
+import Button from 'react-bootstrap/Button';
 import SocialButton from '../_finder/SocialButton';
 import { SERVICES, PLACES, PAGES, ACTORS, FooterType, Paths } from '@/constant';
 import styles from '@/styles/footer/Footer.module.scss';
-import Button from 'react-bootstrap/Button';
+import { Col, Row } from 'react-bootstrap';
 
-export default function Footer() {
-  function renderRow(rowData: FooterType): JSX.Element {
+const socialMedia = [
+  { id: 1, name: "facebook", path: "#" },
+  { id: 2, name: "twitter", path: "#" },
+  { id: 3, name: "viber", path: "#" },
+  { id: 4, name: "telegram", path: "#" },
+]
+
+function Footer() {
+  function renderSocialBtn() {
+    return socialMedia.map(({ id, name, path }) => (
+      <li>
+        <SocialButton
+          href={path}
+          variant="solid"
+          brand={name}
+          roundedCircle
+          className="me-2 mb-2"
+          size=""
+          light=""
+        />
+      </li>
+    ))
+  }
+
+  function renderRow(rowData: FooterType, button: boolean) {
     return (
-      <div className={styles.footer_row + ' mb-sm-0 mb-4'}>
-        <h4 className="h5 d-flex flex-column align-items-center d-md-block">
-          {rowData.title}
-        </h4>
-        <Nav className="flex-column  d-flex flex-column align-items-center d-md-block">
+      <Nav className={`${styles.footer__column} col`}>
+        <h2 className={styles.footer__column_title}>{rowData.title}</h2>
+        <ul>
           {rowData.data.map((item) => (
-            <Nav.Item className="mb-2" key={item.id}>
-              <Nav.Link
-                as={Link}
-                href={item.url}
-                active={false}
-                className="p-0 fw-normal"
-              >
+            <li className="mb-2" key={item.id}>
+              <Link href={item.url} className="nav-link p-0 fw-normal">
                 {item.name}
-              </Nav.Link>
-            </Nav.Item>
+              </Link>
+            </li>
           ))}
-        </Nav>
-      </div>
+          {button &&
+            <li>
+              <Button
+              // @ts-ignore: bootstrap bag*
+                as={Link}
+                href={Paths.AddChoicePage}
+                className="d-flex mx-auto me-md-0">
+                Хочу быть в каталоге
+              </Button>
+            </li>
+          }
+        </ul>
+      </Nav>
     );
   }
 
   return (
-    <div className="pt-4 mt-5">
-      <footer className="footer bg-secondary p-5 mt-5">
-        <Container className="pt-lg-4 p-0">
-          <div className="d-md-flex d-block flex-sm-row flex-column justify-content-between mx-n2">
-            {/* Logo + contacts */}
-            <div className="mb-md-0 mb-4 px-2 d-flex flex-column d-md-block">
+    <footer className="footer bg-secondary mt-5">
+      <Container className={styles.footer__container}>
+        <section id="footer_logo" className={`${styles.footer__logo} mb-4`}>
+          <Link href="/" className="d-block mb-4" >
+            <Image
+              src="/img/header/logo.svg"
+              width={143}
+              height={33}
+              alt="EventForMe"
+              title="Компания EventForMe"
+            />
+          </Link>
+          <ul className={styles.footer__contacts}>
+            <li>
               <Link
-                href="/"
-                className="d-inline-flex mb-4 align-self-center align-self-md-start"
+                href="mailto:example@email.com"
+                className="p-0 fw-normal nav-link"
               >
-                <Image
-                  src="/img/header/logo.png"
-                  width={143}
-                  height={33}
-                  alt="EventForME"
-                />
+                <i className="fi-mail me-2 align-middle opacity-70"></i>
+                example@email.com
               </Link>
+            </li>
+            <li>
+              <Link
+                href="tel:4065550120"
+                className="p-0 fw-normal nav-link"
+              >
+                <i className="fi-device-mobile me-2 align-middle opacity-70"></i>
+                (406) 555-0120
+              </Link>
+            </li>
+            <li className="pt-4">
+              <ul className={styles.footer__social}>
+                {renderSocialBtn()}
+              </ul>
+            </li>
+          </ul>
+        </section>
 
-              <Nav className="flex-column mb-sm-4 mb-2">
-                <Nav.Item
-                  className="mb-2 align-self-center align-self-md-start"
-                  style={{ whiteSpace: 'nowrap' }}
-                >
-                  <Nav.Link
-                    href="mailto:example@email.com"
-                    active={false}
-                    className="p-0 fw-normal"
-                  >
-                    <i className="fi-mail me-2 align-middle opacity-70"></i>
-                    example@email.com
-                  </Nav.Link>
-                </Nav.Item>
+        <Row className="m-0">
 
-                <Nav.Item
-                  style={{ whiteSpace: 'nowrap' }}
-                  className="align-self-center align-self-md-start"
-                >
-                  <Nav.Link
-                    href="tel:4065550120"
-                    active={false}
-                    className="p-0 fw-normal"
-                  >
-                    <i className="fi-device-mobile me-2 align-middle opacity-70"></i>
-                    (406) 555-0120
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
+          {renderRow(SERVICES, false)}
 
-              <div className="pt-2  align-self-center align-self-md-start">
-                <SocialButton
-                  href="#"
-                  variant="solid"
-                  brand="facebook"
-                  roundedCircle
-                  className="me-2 mb-2"
-                  size=""
-                  light=""
-                />
-                <SocialButton
-                  href="#"
-                  variant="solid"
-                  brand="twitter"
-                  roundedCircle
-                  className="me-2 mb-2"
-                  size=""
-                  light=""
-                />
-                <SocialButton
-                  href="#"
-                  variant="solid"
-                  brand="viber"
-                  roundedCircle
-                  className="me-2 mb-2"
-                  size=""
-                  light=""
-                />
-                <SocialButton
-                  href="#"
-                  variant="solid"
-                  brand="telegram"
-                  roundedCircle
-                  className="mb-2"
-                  size=""
-                  light=""
-                />
-              </div>
-            </div>
+          {renderRow(PAGES, false)}
 
-            <div
-              className={
-                styles.footer_rows + ' w-100 w-md-75 justify-content-md-between'
-              }
-            >
-              {renderRow(SERVICES)}
+          {renderRow(PLACES, false)}
 
-              {renderRow(PAGES)}
+          {renderRow(ACTORS, true)}
 
-              {renderRow(PLACES)}
+        </Row>
 
-              {renderRow(ACTORS)}
-            </div>
-          </div>
-          <Nav.Link as={Link} href={Paths.AddChoicePage} active={false}>
-            <Button className="d-flex mx-auto me-md-0">
-              Хочу быть в каталоге
-            </Button>
-          </Nav.Link>
-          {/* Copyright */}
-          <div className={styles.footer_copyright + ' align-items-center'}>
-            <p style={{ whiteSpace: 'nowrap', margin: '0px' }}>© EventForMe</p>
-            <Link href="#">Условия использования</Link>
-            <Link href="#">Политика конфиденциальности</Link>
-          </div>
-        </Container>
-      </footer>
-    </div>
+        {/* Copyright */}
+        <div id="footer_copyright" className={styles.footer__copyright}>
+          <section>© EventForMe</section>
+          <nav>
+            <ul className={styles.footer__conditions} >
+              <li><Link href="#" className={styles.footer__conditions_link}>Условия пользования</Link></li>
+              <li><Link href="#" className={styles.footer__conditions_link}>Политика конфиденциальности</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
+    </footer>
   );
 }
+
+export default Footer;
