af0c335e93644a2e6b41ecffa1bba882c9418375
Ksenia <naomi355@yandex.ru>
2023-07-05 20:15:54 +0400

    модалка в регистрации на главной странице

--- src/components/main/registerBtn/registerBtn.tsx
+++ src/components/main/registerBtn/registerBtn.tsx
@@ -3,21 +3,54 @@ import Link from 'next/link';
 import styles from '@/styles/main/Main.module.scss';
 import Image from 'next/image';
 import { Paths } from '@/constant';
+import SignUp from '@/components/sign/signUp/SignUp';
+import { MouseEvent, useState } from 'react';
+import SignIn from '@/components/sign/signIn/SignIn';
 
-//это кнопка для регистрации на главной станицы
-//вопрос - как сделать так, чтобы при ее нажатии открыть модалку, и не плодить лишний код
+//это кнопка для регистрации на главной станицы, отличается размерами от той, что в хедере
 function RegisterBtn() {
+  // Sign in modal
+  const [signinShow, setSigninShow] = useState(false);
+
+  // Sign up modal
+  const [signupShow, setSignupShow] = useState(false);
+
+  // Swap modals
+  const handleSignInToUp = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setSigninShow(false);
+    setSignupShow(true);
+  };
+  const handleSignUpToIn = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setSigninShow(true);
+    setSignupShow(false);
+  };
+
   return (
-    <Button
-      size="lg"
-      // @ts-ignore: bootstrap bag*
-      //as={Link}
-      //href={Paths.SignUp}
-      className={styles.btn__link}
-    >
-      Регистрация
-      <i className="fi-chevron-right ms-2 fs-xs"></i>
-    </Button>
+    <>
+      <SignUp
+        show={signupShow}
+        onHide={() => setSignupShow(false)}
+        onSwap={handleSignUpToIn}
+      />
+      <SignIn
+        show={signinShow}
+        onHide={() => setSigninShow(false)}
+        onSwap={handleSignInToUp}
+      />
+      <Button
+        size="lg"
+        // @ts-ignore: bootstrap bag*
+        //as={Link}
+        //href={Paths.SignUp}
+        onClick={() => setSignupShow(true)}
+        className={styles.btn__link}
+      >
+        Регистрация
+        <i className="fi-chevron-right ms-2 fs-xs"></i>
+      </Button>
+    </>
   );
 }
 
